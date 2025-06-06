9836846cd18bf34f1f28f48858423262c7d09d7e
Ekaterina <ridiculousprince@gmail.com>
2025-06-06 16:40:15 +0500

    added readme, edited recognition code for better work with cuda

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # cv_experiments
+ Чтобы запустить на gpu:
+
+ Для проверки выполнить в python:
+
+'''
+import torch
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
+'''
+
+
+Если torch.cuda.is_available() выдаёт False, тебе нужно установить PyTorch с поддержкой GPU.
+Перейди на https://pytorch.org, выбери:
+Stable, Pip, Python, CUDA 11.8 (или 12.1 — зависит от версии твоих драйверов)
+
+У меня сработало:
+'''
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+'''
+
+Удалить и потом установить заново:
+
+Удаляем текущую версию PyTorch
+'''
+pip uninstall torch torchvision torchaudio
+'''
+и затем установить снова (см версию выше)
--- coco_recognition.py
+++ coco_recognition.py
@@ -1,17 +1,27 @@
 from ultralytics import YOLO
+import torch
 import cv2
 
-# Загружаем модель
-model = YOLO("yolov8x.pt")  # самая лёгкая версия
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Using device: {device}")
 
-# Камера
-cap = cv2.VideoCapture(0)
+# Загружаем модель и указываем использование GPU
+model = YOLO("yolov8x.pt")
+model.to(device)
+
+cap = cv2.VideoCapture(1)
+
 while True:
     ret, frame = cap.read()
-    results = model(frame)
-    annotated_frame = results[0].plot()
-    cv2.imshow("Detection", annotated_frame)
+    if not ret:
+        break
+
+    results = model.predict(source=frame, device=0, show=False)
+    annotated = results[0].plot()
+    cv2.imshow("Detection", annotated)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
